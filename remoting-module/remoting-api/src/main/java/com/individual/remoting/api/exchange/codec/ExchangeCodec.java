7aa9226cc91fbd9259812d529a4d7ae93cc4b3ed
zhuqi <zhuqi@youzan.com>
2021-06-25 22:35:29 +0800

    code

--- remoting-module/remoting-api/src/main/java/com/individual/remoting/api/exchange/codec/ExchangeCodec.java
+++ remoting-module/remoting-api/src/main/java/com/individual/remoting/api/exchange/codec/ExchangeCodec.java
@@ -9,7 +9,14 @@ import java.io.IOException;
 
 /**
  * 交换层 编解码
+ * <p> 2+1+1+8+4 =16字节 1
+ * +----------------------------------------------------------------+|
+ * | 2 magic | 1 flag |1 status | 8 invokerId | 4 body length | body |
+ * | 2       | seril  | 1       |  8          |  4            |   n  |
+ * +-----------------------------------------------------------------+
  */
+
+
 public class ExchangeCodec implements Codec {
 
     /**
@@ -22,12 +29,12 @@ public class ExchangeCodec implements Codec {
     @Override
     public void encode(ChannelBuffer buffer, Object message) throws IOException {
         if (message instanceof Request) {
-            Request request= (Request) message;
-            encodeRequest(buffer,request);
+            Request request = (Request) message;
+            encodeRequest(buffer, request);
 
         } else if (message instanceof Response) {
-            Response response= (Response) message;
-            encodeResponse(buffer,response);
+            Response response = (Response) message;
+            encodeResponse(buffer, response);
         }
     }
 
@@ -35,7 +42,7 @@ public class ExchangeCodec implements Codec {
     }
 
     private void encodeRequest(ChannelBuffer buffer, Request request) {
-        
+
     }
 
 
