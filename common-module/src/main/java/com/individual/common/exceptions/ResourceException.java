95a42e73b6b9d8da7579e321c2b4949b8ff6527f
zhuqi <zhuqi@youzan.com>
2021-11-18 17:33:34 +0800

    extension

---
+++ common-module/src/main/java/com/individual/common/exceptions/ResourceException.java
@@ -0,0 +1,15 @@
+package com.individual.common.exceptions;
+
+/**
+ * @ProjectName: qee-cloud
+ * @Package: com.individual.common.exceptions
+ * @ClassName: ResourceException
+ * @Description:
+ * @Date: 2021/11/18 4:31 下午
+ * @Version: 1.0
+ */
+public class ResourceException extends RuntimeException {
+    public ResourceException(String message) {
+        super(message);
+    }
+}
